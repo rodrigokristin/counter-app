3ced37ee1980c2a8291891fddcc1a380c424a35d
Rodrii <83188349+rodrigokristin@users.noreply.github.com>
2021-06-24 13:20:34 +0200

    Update script-counterapp.js

--- js/script-counterapp.js
+++ js/script-counterapp.js
@@ -1,20 +1,51 @@
-// set counter
+/* Value Display */
+// create main div in container div
+let divMain = document.createElement('div');
+divMain.setAttribute('id', 'main');
+document.getElementById('container').appendChild(divMain);
+
+// create minus button in main div
+let btnMinus = document.createElement('button');
+btnMinus.setAttribute('id', 'minus');
+btnMinus.innerText = '-';
+divMain.appendChild(btnMinus);
+
+// create value div in main div
+let divValue = document.createElement('div');
+divValue.setAttribute('id', 'value');
+divValue.innerText = '0';
+divMain.appendChild(divValue);
+
+// create plus button in main div
+let btnPlus = document.createElement('button');
+btnPlus.setAttribute('id', 'plus');
+btnPlus.innerText = '+';
+divMain.appendChild(btnPlus);
+
+// create reset button in container div
+let btnReset = document.createElement('button');
+btnReset.setAttribute('id', 'reset');
+btnReset.innerText = 'Reset';
+document.getElementById('container').appendChild(btnReset);
+
+/* Value Change */
+// create counter
 let counter = 0;
 
-// increases value
-function increase() {
-  counter++;
-  document.getElementById('value').innerText = counter;
-}
-
-// decreases value
-function decrease() {
+// addEventListener to btnMinus
+btnMinus.addEventListener('click', () => {
   counter--;
-  document.getElementById('value').innerText = counter;
-}
+  divValue.innerText = counter;
+});
 
-// reset value to zero
-function reset() {
+//addEventListener to btnPlus
+btnPlus.addEventListener('click', () => {
+ counter++;
+ divValue.innerText = counter;
+});
+
+//addEventListener to btnReset
+btnReset.addEventListener('click', () => {
   counter = 0;
-  document.getElementById('value').innerText = counter;
-}
+  divValue.innerText = counter;
+});
